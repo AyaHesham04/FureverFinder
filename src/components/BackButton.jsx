448e9e3cebb8f7781d416b136a9cfab604338c56
AyaHesham04 <ayahisham04@gmail.com>
2024-12-04 22:38:03 +0200

    pet details page

--- src/components/BackButton.jsx
+++ src/components/BackButton.jsx
@@ -6,12 +6,12 @@ function BackButton({ redirectTo = "/" }) {
   return (
     <Link
       to={redirectTo} // Replace with the desired back link
-      className="items-center absolute left-[15%] top-[5%] z-10"
+      className="items-center absolute left-[13%] top-[5%] z-10"
     >
       <img
         src={backArrow}
         alt="Back"
-        className="w-12 h-12 max-[430px]:w-8 max-[430px]:h-8 transition-transform hover:scale-110"
+        className="2xl:w-10 2xl:h-10 xl:w-10 xl:h-10 lg:w-10 lg:h-10 md:w-9 md:h-9 sm:w-8 sm:h-8 max-[430px]:w-7 max-[430px]:h-7 transition-transform hover:scale-110"
       />
     </Link>
   );
