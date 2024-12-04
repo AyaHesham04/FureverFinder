98ba75a0b8d82d9b5e98d2e85e8bd42544ff9411
AyaHesham04 <ayahisham04@gmail.com>
2024-12-04 20:31:49 +0200

    pet details photo list

--- src/components/MediaPreviewList.jsx
+++ src/components/MediaPreviewList.jsx
@@ -17,19 +17,19 @@ const MediaSlider = ({ mediaList }) => {
 
     return (
         <div className="relative w-full max-w-lg mx-auto">
-            <div className="overflow-hidden rounded-lg shadow-lg">
+            <div className="overflow-hidden rounded-lg shadow-lg 2xl:w-96 2xl:h-72 xl:w-80 xl:h-60 lg:w-72 lg:h-52 md:w-64 md:h-48 sm:w-60 sm:h-44 max-[430px]:w-36 max-[430px]:h-32 max-[400px]:w-32 max-[400px]:h-28">
                 {mediaList[currentIndex].type === "image" && (
                     <img
                         src={mediaList[currentIndex].url}
                         alt={`Slide ${currentIndex}`}
-                        className="w-full h-auto"
+                        className="w-full h-full"
                     />
                 )}
                 {mediaList[currentIndex].type === "video" && (
                     <video
                         src={mediaList[currentIndex].url}
                         controls
-                        className="w-full h-auto"
+                        className="w-full h-full"
                     ></video>
                 )}
             </div>
@@ -37,13 +37,13 @@ const MediaSlider = ({ mediaList }) => {
             {/* Navigation Controls */}
             <button
                 onClick={goToPrev}
-                className="absolute top-1/2 left-2 transform -translate-y-1/2 bg-black bg-opacity-50 text-white p-2 rounded-full"
+                className="absolute top-1/2 left-2 transform -translate-y-1/2 bg-black bg-opacity-50 text-white p-1 rounded-full"
             >
                 &larr;
             </button>
             <button
                 onClick={goToNext}
-                className="absolute top-1/2 right-2 transform -translate-y-1/2 bg-black bg-opacity-50 text-white p-2 rounded-full"
+                className="absolute top-1/2 right-2 transform -translate-y-1/2 bg-black bg-opacity-50 text-white p-1 rounded-full"
             >
                 &rarr;
             </button>
@@ -53,7 +53,7 @@ const MediaSlider = ({ mediaList }) => {
                 {mediaList.map((_, index) => (
                     <button
                         key={index}
-                        className={`w-3 h-3 mx-1 rounded-full ${index === currentIndex
+                        className={`w-2 h-2 mx-1 rounded-full ${index === currentIndex
                             ? "bg-pink-light"
                             : "bg-gray-300 hover:bg-gray-400"
                             }`}
--- src/pages/PetDetailsPage.jsx
+++ src/pages/PetDetailsPage.jsx
@@ -1,5 +1,8 @@
 import background from "../assets/PetDetailsPage/PetDetailsBack.png";
 import temp1 from "../assets/TemporaryImages/cat1.jpg";
+import temp2 from "../assets/TemporaryImages/cat2.jpg";
+import temp3 from "../assets/TemporaryImages/cat3.jpg";
+import BackButton from "../components/BackButton";
 import MediaSlider from "../components/MediaPreviewList";
 
 const PetDetails = () => {
@@ -10,8 +13,17 @@ const PetDetails = () => {
     { url: "https://www.w3schools.com/html/movie.mp4", type: "video" },
   ];
   return (
-    <div className="min-h-screen bg-gray-50 flex items-center justify-center">
-      <MediaSlider mediaList={mediaData} />
+    <div className="relative w-screen h-screen overflow-hidden">
+      <BackButton></BackButton>
+      <img src={background} alt="Background" className="w-full h-full" />
+      <div className="w-full h-full flex flex-col justify-center items-center">
+        <div className="bg-[#FFE5E6] flex items-center justify-center absolute top-14 p-6 rounded-lg shadow-lg">
+          <MediaSlider mediaList={mediaData} />
+        </div>
+        <h1 className="text-[#5F5B5B] absolute max-[400px]:top-[16rem] max-[430px]:top-[17rem] sm:top-80 md:top-[22rem] lg:top-96 xl:top-96 2xl:top-80 font-poppins font-semibold text-[28px] max-[430px]:text-[20px] lg:text-[27px] xl:text-[30px] 2xl:text-[30px]">
+          Categories
+        </h1>
+      </div>
     </div>
   );
 }
